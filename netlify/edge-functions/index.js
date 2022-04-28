841663ed80f27e2cbed706049d856261899fd234
Nicholas Jitkoff <nicholas@jitkoff.com>
2022-04-27 21:15:02 -0600

    remove debug messages

--- netlify/edge-functions/index.js
+++ netlify/edge-functions/index.js
@@ -1,6 +1,5 @@
 // import { Context } from "netlify:edge";
 
-
 export default async (request, context) => {
   const agent = request.headers.get("user-agent");
   let url = new URL(request.url);
@@ -13,18 +12,17 @@ export default async (request, context) => {
     let title = decodeURIComponent(components.shift()).replace("_", " ");
     let desc = decodeURIComponent(components.shift()).replace("_", " ");
     let image = decodeURIComponent(components.join("/"));
-    console.log("components", title, desc, image)
 
     let content = "";
     if (title) content += `<title>${title}</title><meta property="og:title" content="${title}"/>`;
     if (desc && desc.length > 1) content += `<meta name="description" content="${desc}"/><meta property="og:description" content="${desc}"/>`;
     if (image) {
       if (image.startsWith("http")) {
-        console.debug("URL Image", image)
+        //console.debug("URL Image", image)
         content += `<meta property="og:image" content="${image}"/>`;
       } else {
         image = decodeURIComponent(image)
-        console.log("Emoji Image", image);
+        //console.log("Emoji Image", image);
         let codepoints = [];
         for (const char of image) {
           codepoints.push(char.codePointAt(0).toString(16));
@@ -32,7 +30,6 @@ export default async (request, context) => {
         content += `<link rel="icon" type="image/png" href="https://fonts.gstatic.com/s/e/notoemoji/14.0/${codepoints.join("_")}/128.png">`
       }
     }
-    console.log("content", content)
     return new Response(content, {
       headers: { "content-type": "text/html" },
     });
